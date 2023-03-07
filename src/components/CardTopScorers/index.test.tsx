82cfd1ee49be2685d4671519bca95a9d7754af9d
lucas <lucasfrutig0@gmal.com>
2023-03-07 10:54:00 -0300

    add test to components

--- src/components/CardTopScorers/index.test.tsx
+++ src/components/CardTopScorers/index.test.tsx
@@ -1,13 +1,12 @@
 import { screen } from "@testing-library/react";
 import { describe } from "vitest";
-import { Hero } from ".";
+import { CardTopScorers } from ".";
 import { setup } from "../../utils/setupTestRender";
 
-describe("Hero Component", () => {
+describe("CardTopScorers Component", () => {
   it("should render without crash", () => {
-    setup(<Hero />);
-    expect(
-      screen.getByRole("heading", { name: /premier league/i })
-    ).toBeInTheDocument();
+    setup(<CardTopScorers />);
+
+    expect(screen.getByText("PSG")).toBeInTheDocument();
   });
 });
