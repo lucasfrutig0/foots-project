6bd3a9f3d0afdc23bd806d098a9eab59eb7798f2
lucas <lucasfrutig0@gmal.com>
2023-03-08 16:40:54 -0300

    feature: add sidebar menu responsive

---
+++ src/mocks/data/menu.ts
@@ -0,0 +1,48 @@
+import premier_league_logo from "../../assets/images/premierleague.svg";
+import la_liga from "../../assets/images/laliga2-logo.svg";
+import ligue_1 from "../../assets/images/ligue1_menu.svg";
+import bundesliga_logo from "../../assets/images/bundeslogo_menu.svg";
+import champions from "../../assets/images/uefa-logo.svg";
+import europa_league from "../../assets/images/europa_league.svg";
+import libertadores from "../../assets/images/liberta.svg";
+import brasileiro from "../../assets/images/brasileiraook.svg";
+import copa_do_brasil from "../../assets/images/copadobrasil_logo.svg";
+
+export const navLinksItems = [
+  {
+    slug: '/premier_league',
+    icon: premier_league_logo
+  },
+  {
+    slug: '/la_liga',
+    icon: la_liga
+  },
+  {
+    slug: '/ligue_1',
+    icon: ligue_1
+  },
+  {
+    slug: '/bundesliga',
+    icon: bundesliga_logo
+  },
+  {
+    slug: '/champions',
+    icon: champions
+  },
+  {
+    slug: '/europa_league',
+    icon: europa_league
+  },
+  {
+    slug: '/libertadores',
+    icon: libertadores
+  },
+  {
+    slug: '/brasileiro',
+    icon: brasileiro
+  },
+  {
+    slug: '/copa_do_brasil',
+    icon: copa_do_brasil
+  }
+]
