1a5ae79c8fa4ab48497b85fa8d562f57ae133277
lucas <lucasfrutig0@gmal.com>
2023-03-09 10:58:09 -0300

    feature: add standing table

---
+++ src/components/StandingTable/index.tsx
@@ -0,0 +1,278 @@
+export const StandingTable = () => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-compact w-full">
+        <thead className="text-center">
+          <tr>
+            <th></th>
+            <th>T</th>
+            <th>J</th>
+            <th>V</th>
+            <th>E</th>
+            <th>D</th>
+            <th>GP</th>
+            <th>GS</th>
+            <th>SG</th>
+            <th>PTS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th className="text-center">1</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">2</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">3</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">4</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">5</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">6</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">7</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">8</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">9</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">10</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">11</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">12</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">13</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">14</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">15</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">16</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">17</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">18</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">19</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+          <tr>
+            <th className="text-center">20</th>
+            <td className="text-center">Arsenal</td>
+            <td className="text-center">21</td>
+            <td className="text-center">12</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">2</td>
+            <td className="text-center">7</td>
+            <td className="text-center">22</td>
+            <td className="text-center">52</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr className="text-center">
+            <th></th>
+            <th>T</th>
+            <th>J</th>
+            <th>V</th>
+            <th>E</th>
+            <th>D</th>
+            <th>GP</th>
+            <th>GS</th>
+            <th>SG</th>
+            <th>PTS</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
